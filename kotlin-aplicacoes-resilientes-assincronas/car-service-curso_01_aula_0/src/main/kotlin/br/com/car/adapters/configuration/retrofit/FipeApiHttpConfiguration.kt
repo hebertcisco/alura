325578eb9b4750475e9cc0ae9e5e51f634086e0c
hebertcisco <hebertcisco@outlook.com>
2025-01-11 12:21:11 -0300

    Rename FipeApiHttpConfiguration and inject CircuitBreakerConfiguration for improved resilience

--- kotlin-aplicacoes-resilientes-assincronas/car-service-curso_01_aula_0/src/main/kotlin/br/com/car/adapters/configuration/retrofit/FipeApiHttpConfiguration.kt
+++ kotlin-aplicacoes-resilientes-assincronas/car-service-curso_01_aula_0/src/main/kotlin/br/com/car/adapters/configuration/retrofit/FipeApiHttpConfiguration.kt
@@ -1,6 +1,8 @@
-package br.com.car.adapters.api.configuration.retrofit
+package br.com.car.adapters.configuration.retrofit
 
+import br.com.car.adapters.configuration.circuitbreaker.CircuitBreakerConfiguration
 import br.com.car.adapters.http.FipeApiHttpService
+import io.github.resilience4j.retrofit.CircuitBreakerCallAdapter
 import okhttp3.OkHttpClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -8,7 +10,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 @Configuration
-class FipeApiHttpConfiguration {
+class FipeApiHttpConfiguration(
+    private val circuitBreakerConfiguration: CircuitBreakerConfiguration
+) {
     private companion object {
         const val BASE_URL = "https://parallelum.com.br"
     }
@@ -16,6 +20,7 @@ class FipeApiHttpConfiguration {
     private fun buildClient() = OkHttpClient.Builder().build()
 
     private fun buildRetrofit() = Retrofit.Builder()
+        .addCallAdapterFactory(CircuitBreakerCallAdapter.of(circuitBreakerConfiguration.getCircuitBreaker()))
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .client(buildClient())
