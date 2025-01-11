87025f4761bb8a94ecd8f06680d7f78c3c40d001
hebertcisco <hebertcisco@outlook.com>
2025-01-10 22:17:41 -0300

    Remove unnecessary blank line in CarService interface

--- kotlin-aplicacoes-resilientes-assincronas/car-service-curso_01_aula_0/src/main/kotlin/br/com/car/domain/ports/CarService.kt
+++ kotlin-aplicacoes-resilientes-assincronas/car-service-curso_01_aula_0/src/main/kotlin/br/com/car/domain/ports/CarService.kt
@@ -3,7 +3,6 @@ package br.com.car.domain.ports
 import br.com.car.domain.model.Car
 
 interface CarService {
-
     fun list(model: String?): List<Car>
     fun save(car: Car): Int
     fun update(car: Car, id: Long): Int
