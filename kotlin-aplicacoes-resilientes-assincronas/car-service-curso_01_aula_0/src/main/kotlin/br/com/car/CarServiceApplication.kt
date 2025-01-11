bb697811cf2ad13514d5faf3a6af958d56ccdee7
hebertcisco <hebertcisco@outlook.com>
2025-01-10 22:17:55 -0300

    Refactor CarServiceApplication to enable scheduling and replace ComponentScan with Configuration annotation

--- kotlin-aplicacoes-resilientes-assincronas/car-service-curso_01_aula_0/src/main/kotlin/br/com/car/CarServiceApplication.kt
+++ kotlin-aplicacoes-resilientes-assincronas/car-service-curso_01_aula_0/src/main/kotlin/br/com/car/CarServiceApplication.kt
@@ -3,11 +3,13 @@ package br.com.car
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
-import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableCaching
 @SpringBootApplication
-@ComponentScan("br.com.car")
+@Configuration
+@EnableScheduling
 class CarServiceApplication
 
 fun main(args: Array<String>) {
